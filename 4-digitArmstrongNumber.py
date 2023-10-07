09982ab4ca1a4690785bc397e41d979e34454ed2
L V S Janaki Rama Raju <138346986+LVSJanakiRamaraju@users.noreply.github.com>
2023-10-07 06:50:30 +0530

    4-digit Armstrong Number
    Write a program that reads a four-digit number N and checks if N is an Armstrong Number.
    Print Armstrong Number is N is an Armstrong number. Otherwise, print Not an Armstrong Number.

---
+++ 4-digitArmstrongNumber.py
@@ -0,0 +1,10 @@
+N=input()
+N1=int(N[0])**4
+N2=int(N[1])**4
+N3=int(N[2])**4
+N4=int(N[3])**4
+N=int(N)
+if N1+N2+N3+N4==N:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
